a1255c5bf947bde1eedcd76f186070866b5a4da0
kkotes <seminae99@gmail.com>
2024-12-08 21:27:17 +0300

    feat: solve 56 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -645,8 +645,15 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let count = 0;
+  const num = Math.abs(number);
+  for (let i = 1; i <= num; i += 1) {
+    if (i % 2 !== 0) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
